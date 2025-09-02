f2b4a591334440df4ab11df51e5640acb80c9406
YusifValiyev <yusifvaliyev903@gmail.com>
2025-09-02 13:45:34 +0400

    Backend added

--- src/components/voice/Vapi.jsx
+++ src/components/voice/Vapi.jsx
@@ -1,21 +1,8 @@
 import { VapiWidget } from "@vapi-ai/client-sdk-react";
 import { useEffect } from "react";
+import "./index.css";
 
 export default function Vapi() {
-    useEffect(() => {
-        async function getData() {
-            const response = await fetch(
-                "https://primary-production-ccc3.up.railway.app/webhook-test/blueone",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-        }
-        getData();
-    }, []);
     const handleCallStart = () => {
         console.log("Voice call started");
     };
@@ -24,9 +11,7 @@ export default function Vapi() {
         console.log("Voice call ended");
     };
 
-    const handleMessage = (message) => {
-        console.log("Message received:", message);
-    };
+    const handleMessage = async (message) => {};
 
     const handleError = (error) => {
         console.error("Widget error:", error);
@@ -36,17 +21,12 @@ export default function Vapi() {
         <VapiWidget
             publicKey="8118bbe9-dae4-40c5-b7af-30300a1539be"
             assistantId="55389fd7-ee50-4c73-9cee-7bd063c11979"
-            assistantOverrides={{
-                variableValues: { name: "John" },
-            }}
-            mode="hybrid"
-            consentRequired={true}
-            consentTitle="Privacy Agreement"
-            consentContent="By using this service, you agree to our terms..."
-            title="Support Assistant"
-            hybridEmptyMessage="Start a conversation with voice or text"
+            mode="voice"
             onMessage={handleMessage}
             onError={handleError}
+            ctaButtonColor="#000"
+            voiceShowTranscript={true}
+            accentColor="#a282ff"
         />
     );
 }
